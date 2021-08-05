901b44216129e8d664708891293d9d399e715caa
Itsupera <itsupera@gmail.com>
2021-08-05 21:43:25 +0200

    Separate typer app from main function

--- yttt.py
+++ yttt.py
@@ -1,17 +1,18 @@
 import csv
 import datetime
 import json
+import logging
 import os
 import subprocess as sp
 import sys
 from collections import Counter, defaultdict
-from pathlib import Path
+from tempfile import TemporaryDirectory
 from typing import Optional
 
 import typer
 
 
-def main(
+def typer_app(
     watch_history_json_filepath: str = typer.Argument(..., help="Path to the watch-history.json file"),
     output_filepath: str = typer.Argument(..., help="Path to write the CSV file to"),
     date_from: Optional[str] = typer.Option(None, help="YYYY-MM-DD formated date to filter history "
@@ -19,10 +20,22 @@ def main(
     date_to: Optional[str] = typer.Option(None, help="YYYY-MM-DD formated date to filter history to "
                                                      "(default: today, must be after date_from)"),
 ):
+    main(watch_history_json_filepath, output_filepath, date_from=date_from, date_to=date_to, interactive=True)
+
+
+def main(
+    watch_history_json_filepath,
+    output_filepath,
+    date_from=None,
+    date_to=None,
+    interactive=False,
+):
     """
     Extract stats from your Youtube history and write a CSV file 
     with the total duration for each day and channel name.
     """
+    logging.info("Starting...")
+
     if date_to:
         date_to = datetime.datetime.strptime(date_to, "%Y-%m-%d")
     else:
@@ -32,41 +45,39 @@ def main(
         date_from = datetime.datetime.strptime(date_from, "%Y-%m-%d")
         if date_from > date_to:
             raise ValueError(f"date_from must be older than date_to ({date_to.strftime('%Y-%m-%d')}) !")
-    else:
-        confirm = input("No --date-from given, are you sure you want to compute stats for your entire Youtube history ? "
-                        "(This may take a while) [Y/n]")
+    elif interactive:
+        confirm = input("No --date-from given, are you sure you want to compute stats "
+                        "for your entire Youtube history ? (This may take a while) [Y/n]")
         if confirm.lower() != "y":
             sys.exit(0)
 
     with open(watch_history_json_filepath) as fd:
-            history = json.load(fd)
+        history = json.load(fd)
 
     stats = defaultdict(lambda: Counter())
-    stats_by_channel =  Counter()
+    stats_by_channel = Counter()
 
-    metadatas_fpath = "metadatas"
-    Path(metadatas_fpath).mkdir(exist_ok=True, parents=True)
+    with TemporaryDirectory(prefix="yttt_metadatas") as metadatas_fpath:
+        for idx, entry in enumerate(history):
+            dt = parse_entry_time(entry)
+            if date_from and dt < date_from:
+                break
+            elif dt > date_to:
+                continue
 
-    for idx, entry in enumerate(history):
-        dt = datetime.datetime.fromisoformat(entry['time'].rstrip("Z"))
-        if date_from and dt < date_from:
-            break
-        elif dt > date_to:
-            continue
+            url_path = entry['titleUrl']
+            prefix_fpath = os.path.join(metadatas_fpath, f"{str(idx + 1).zfill(5)}")
+            download_video_metadata(url_path, prefix_fpath)
+            metadata_fpath = prefix_fpath + ".info.json"
 
-        url_path = entry['titleUrl']
-        prefix_fpath = os.path.join(metadatas_fpath, f"{str(idx + 1).zfill(5)}")
-        download_video_metadata(url_path, prefix_fpath)
-        metadata_fpath = prefix_fpath + ".info.json"
+            with open(metadata_fpath) as fd:
+                metadata = json.load(fd)
 
-        with open(metadata_fpath) as fd:
-            metadata = json.load(fd)
-
-        channel_name = metadata['channel']
-        duration_in_minutes = round(metadata['duration'] / 60)
-        day = dt.strftime("%Y-%m-%d")
-        stats[day][channel_name] += duration_in_minutes
-        stats_by_channel[channel_name] += duration_in_minutes
+            channel_name = metadata['channel']
+            duration_in_minutes = round(metadata['duration'] / 60)
+            day = dt.strftime("%Y-%m-%d")
+            stats[day][channel_name] += duration_in_minutes
+            stats_by_channel[channel_name] += duration_in_minutes
 
     if not date_from:
         date_from = dt
@@ -87,6 +98,8 @@ def main(
             row = {"Day": day, "TOTAL": sum(channel_to_duration.values()), **channel_to_duration}
             csv_writer.writerow(row)
 
+    logging.info("DONE !")
+
 
 def download_video_metadata(url_path, prefix_fpath):
     cmd = f'youtube-dl -o "{prefix_fpath}" --skip-download --write-info-json -i {url_path}'
@@ -94,8 +107,22 @@ def download_video_metadata(url_path, prefix_fpath):
     line = True
     while line:
         line = p.stdout.readline().decode("utf-8").strip()
-        print(line)
+        logging.info(line)
+
+
+def summarize_history_stats(history):
+    if not history:
+        return "Empty watch history"
+
+    nb_videos = len(history)
+    oldest_day = parse_entry_time(history[-1]).strftime("%Y-%m-%d")
+    newest_day = parse_entry_time(history[0]).strftime("%Y-%m-%d")
+    return nb_videos, oldest_day, newest_day
+
+
+def parse_entry_time(entry):
+    return datetime.datetime.fromisoformat(entry['time'].rstrip("Z"))
 
 
 if __name__ == "__main__":
-    typer.run(main)
+    typer.run(typer_app)
